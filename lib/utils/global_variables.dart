cbd312bfd182a16270ced643dfaecdc905353c75
Bedoui <firas-bedoui@hotmail.fr>
2022-06-15 22:52:49 +0200

    Update global with homeScreenItems

---
+++ lib/utils/global_variables.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:marlonne/screens/add_post_screen.dart';
+
+const webSceenSize = 600;
+
+const homeScreenItems = [
+  Text('feed'),
+  Text('search'),
+  AddPostScreen(),
+  Text('notif'),
+  Text('profile'),
+];
